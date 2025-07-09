efe3260b0747fecf26d07e4134d1ae2f2b419280
Miriam <mduranre@uoc.edu>
2025-07-09 14:57:07 +0200

    Estres server

--- miriam.vue/src/router/index.js
+++ miriam.vue/src/router/index.js
@@ -1,13 +1,14 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
 import Curriculum from '@/views/Curriculum.vue'
 import Fullscat from '../views/projects/fullscat.vue';
 import Javalinos from '../views/projects/javalinos.vue';
 import NewTitans from '../views/projects/newtitans.vue';
-import { createRouter, createWebHistory } from 'vue-router'
+
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory('/CVMiriam/'),
   routes: [
     { path: '/', name: 'home', component: HomeView },
     { path: '/projects', name: 'projects', component: ProjectsView },
--- miriam.vue/vite.config.js
+++ miriam.vue/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CVMiriam/',      
+  base: '/CVMiriam/', 
   plugins: [
     vue(),
     vueDevTools(),
