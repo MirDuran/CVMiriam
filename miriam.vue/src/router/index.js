6595236a153da7a5c1cdfcbe1188b7bc8031ed10
Miriam <mduranre@uoc.edu>
2025-07-09 13:46:41 +0200

    subiendo al servidor

--- miriam.vue/src/router/index.js
+++ miriam.vue/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' 
 import HomeView from '../views/HomeView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
 import Curriculum from '@/views/Curriculum.vue'
@@ -7,38 +7,14 @@ import Javalinos from '../views/projects/javalinos.vue';
 import NewTitans from '../views/projects/newtitans.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),  
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: ProjectsView,
-    },
-    {
-      path: '/cv', 
-      name: 'Curriculum',
-      component: Curriculum
-    },
-    {
-    path: '/projects/fullscat',
-    name: 'fullscat',
-    component: Fullscat,
-  },
-  {
-    path: '/projects/javalinos',
-    name: 'javalinos',
-    component: Javalinos,
-  },
-  {
-    path: '/projects/newtitans',
-    name: 'newtitans',
-    component: NewTitans,
-  }
+    { path: '/', name: 'home', component: HomeView },
+    { path: '/projects', name: 'projects', component: ProjectsView },
+    { path: '/cv', name: 'Curriculum', component: Curriculum },
+    { path: '/projects/fullscat', name: 'fullscat', component: Fullscat },
+    { path: '/projects/javalinos', name: 'javalinos', component: Javalinos },
+    { path: '/projects/newtitans', name: 'newtitans', component: NewTitans },
   ],
 })
 
